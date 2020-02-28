b659e6b1fed58e002f47901931fb8b5a469426c2
Fabian F. Voigt <voigt@hifo.uzh.ch>
2020-02-28 14:54:01 +0100

    Bugfix: Demo camera returned images that are float64, not uint16 - could not be opened in Fiji

--- mesoSPIM/src/mesoSPIM_Camera.py
+++ mesoSPIM/src/mesoSPIM_Camera.py
@@ -362,8 +362,9 @@ class mesoSPIM_DemoCamera(mesoSPIM_GenericCamera):
         self.line = 400*np.sin(self.line)+1200
 
     def _create_random_image(self):
-        data = np.array([np.roll(self.line, 4*i+self.count) for i in range(0, self.y_pixels)])
-        data = data + np.random.normal(size=(self.x_pixels, self.y_pixels))*100
+        data = np.array([np.roll(self.line, 4*i+self.count) for i in range(0, self.y_pixels)], dtype='uint16')
+        data = data + (np.random.normal(size=(self.x_pixels, self.y_pixels))*100)
+        data = np.around(data).astype('uint16')
         self.count += 20
         return data
 
