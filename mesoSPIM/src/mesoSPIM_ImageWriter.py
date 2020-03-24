bugfix: tiling shift units um->px

mesoSPIM/src/mesoSPIM_ImageWriter.py
@@ -65,10 +65,10 @@ class mesoSPIM_ImageWriter(QtCore.QObject):
                                                     ntiles=acq_list.get_n_tiles())
             # x and y need to be exchanged to account for the image rotation
             shape = (self.max_frame, self.y_pixels, self.x_pixels)
-            affine_matrix = np.array(((1.0, 0.0, 0.0, acq['x_pos']),
-                                      (0.0, 1.0, 0.0, acq['y_pos']),
-                                      (0.0, 0.0, 1.0, acq['z_start'])))
             px_size_um = self.cfg.pixelsize[acq['zoom']]
+            affine_matrix = np.array(((1.0, 0.0, 0.0, acq['x_pos']/px_size_um),
+                                      (0.0, 1.0, 0.0, acq['y_pos']/px_size_um),
+                                      (0.0, 0.0, 1.0, acq['z_start']/acq['z_step'])))
             self.bdv_writer.append_view(stack=None, virtual_stack_dim=shape,
                                         illumination=acq_list.find_value_index(acq['shutterconfig'], 'shutterconfig'),
                                         channel=acq_list.find_value_index(acq['laser'], 'laser'),