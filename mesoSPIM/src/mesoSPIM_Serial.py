ceef6068d53d9c6af97029ad29764f9442a1e69d
cs <42037938+drchrisch@users.noreply.github.com>
2021-04-23 17:56:09 +0200

    Update mesoSPIM_Serial.py
    New stage class name

--- mesoSPIM/src/mesoSPIM_Serial.py
+++ mesoSPIM/src/mesoSPIM_Serial.py
@@ -21,7 +21,7 @@ from .mesoSPIM_State import mesoSPIM_StateSingleton
 from .devices.filter_wheels.ludlcontrol import LudlFilterwheel
 from .devices.filter_wheels.mesoSPIM_FilterWheel import mesoSPIM_DemoFilterWheel
 from .devices.zoom.mesoSPIM_Zoom import DynamixelZoom, DemoZoom
-from .mesoSPIM_Stages import mesoSPIM_PIstage, mesoSPIM_PI_xyzf_Stages, mesoSPIM_DemoStage, mesoSPIM_GalilStages, mesoSPIM_PI_f_rot_and_Galil_xyz_Stages, mesoSPIM_PI_rot_and_Galil_xyzf_Stages, mesoSPIM_PI_rotz_and_Galil_xyf_Stages, mesoSPIM_PI_rotzf_and_Galil_xy_Stages
+from .mesoSPIM_Stages import mesoSPIM_PIstage, mesoSPIM_PIStages, mesoSPIM_DemoStage, mesoSPIM_GalilStages, mesoSPIM_PI_f_rot_and_Galil_xyz_Stages, mesoSPIM_PI_rot_and_Galil_xyzf_Stages, mesoSPIM_PI_rotz_and_Galil_xyf_Stages, mesoSPIM_PI_rotzf_and_Galil_xy_Stages
 # from .mesoSPIM_State import mesoSPIM_State
 
 class mesoSPIM_Serial(QtCore.QObject):
@@ -67,8 +67,8 @@ class mesoSPIM_Serial(QtCore.QObject):
         ''' Attaching the stage '''
         if self.cfg.stage_parameters['stage_type'] == 'PI':
             self.stage = mesoSPIM_PIstage(self)
-        elif self.cfg.stage_parameters['stage_type'] == 'PI_xyzf':
-            self.stage = mesoSPIM_PI_xyzf_Stages(self)
+        elif self.cfg.stage_parameters['stage_type'] == 'PIStages':
+            self.stage = mesoSPIM_PIStages(self)
             self.stage.sig_position.connect(lambda dict: self.sig_position.emit({'position': dict}))
         elif self.cfg.stage_parameters['stage_type'] == 'GalilStage':
             self.stage = mesoSPIM_GalilStages(self)
