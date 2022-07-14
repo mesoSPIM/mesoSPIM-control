ab166270bffac3bb092f7975383da305ebf71340
Nikita Vladimirov <nvladimus@gmail.com>
2022-07-14 21:22:50 +0200

    create icon, shortcut, refine bat-file instructions

--- mesoSPIM/start_mesoSPIM.bat
+++ mesoSPIM/start_mesoSPIM.bat
@@ -1,10 +1,12 @@
-rem Change the Anaconda path "C:\Users\Nikita\anaconda3\Scripts\" and environments path "C:\Users\Nikita\anaconda3\envs" to your own
-rem To know your Anaconda path, run in Anaconda prompt:
-rem $ where conda
-rem
-rem In a multi-user setting, you may want to create the py36 environment in a public folder accessible for all users:
-rem $ conda create -p C:/full/public/path/to/envs/py36 python=3.6
-rem Make sure all users have right to execute python in the py36 environment (you may need admin rights for that).
 echo off
-"%windir%\System32\cmd.exe" /k ""C:\ProgramData\Anaconda3\Scripts\activate.bat" "C:\Users\nvladi\.conda\envs\py37-burner" && python "mesoSPIM_Control.py""
+echo Setting up your conda enviroment:
+echo Change the Anaconda path "...\activate.bat" and environment path "...envs\py37" to your own
+echo To print your Anaconda path, run this in Anaconda prompt:
+echo $ where conda
+echo In a multi-user setting, you can create the py37 environment in a public folder accessible for all users:
+echo EXAMPLE:
+echo $ conda create -p C:/Users/Public/conda/envs/py37 python=3.7
+echo Make sure all users have right to execute python in this py37 environment (admin rights required).
+echo off
+"%windir%\System32\cmd.exe" /k ""C:\Users\Nikita\anaconda3\Scripts\activate.bat" "C:\Users\Nikita\anaconda3\envs\py37-burner" && "mesospim""
 pause
