2933e91b3736a60d5b5699949bc968572dcae08d
Nikita Vladimirov <nvladimus@gmail.com>
2023-03-02 00:42:30 +0100

    GUI change Acquisition Manage: M button smaller and moved to the right from table entry.

--- mesoSPIM/src/utils/widgets.py
+++ mesoSPIM/src/utils/widgets.py
@@ -1,47 +1,33 @@
 from PyQt5 import QtWidgets, QtGui, QtCore, QtDesigner
 
+
 class MarkPositionWidget(QtWidgets.QWidget):
     """ Pushbutton plus position indicator """
     pressed = QtCore.pyqtSignal()
 
     def __init__(self, parent=None):
-        #QtWidgets.QWidget.__init__(self, parent)
         super().__init__(parent)
-        # QtDesigner.QPyDesignerCustomWidgetPlugin.__init__(self, parent)
 
-        self.button = QtWidgets.QPushButton()
+        self.button = QtWidgets.QToolButton()
         self.button.setText("M")
 
         font = QtGui.QFont()
-        font.setPointSize(14)
+        font.setPointSize(12)
         self.button.setFont(font)
-
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.button.sizePolicy().hasHeightForWidth())
-        self.button.setSizePolicy(sizePolicy)
-        self.button.setMinimumSize(QtCore.QSize(25, 0))
+        self.button.setMaximumWidth(30)
 
         self.lineEdit = QtWidgets.QLineEdit()
         self.lineEdit.setFont(font)
-        # self.lineEdit.setReadOnly(True)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
-        self.lineEdit.setSizePolicy(sizePolicy)
 
         self.layout = QtWidgets.QHBoxLayout()
-        self.layout.addWidget(self.button)
         self.layout.addWidget(self.lineEdit)
-        ''' '''
+        self.layout.addWidget(self.button)
         self.layout.setContentsMargins(0,0,0,0)
+        self.setLayout(self.layout)
+        self.setAutoFillBackground(True)
 
         self.button.clicked.connect(lambda: self.pressed.emit())
 
-        self.setLayout(self.layout)
-        self.setAutoFillBackground(True)
 
 class SliderWithValue(QtWidgets.QWidget):
     ''' Slider with value to the left '''
