3bc0ee604b80ae5f6068899441d60208a22a704d
TchLenn <80828085+TchLenn@users.noreply.github.com>
2024-11-15 11:36:15 +0000

    Update mesoSPIM_Core.py

--- mesoSPIM/src/mesoSPIM_Core.py
+++ mesoSPIM/src/mesoSPIM_Core.py
@@ -448,7 +448,7 @@ class mesoSPIM_Core(QtCore.QObject):
         shutterconfig = self.state['shutterconfig']
         if shutterconfig == 'Both':
             if self.shutterswitch is False:
-                self.shutter_left.open()
+              #  self.shutter_left.open()
                 self.shutter_right.open()
         elif shutterconfig == 'Left':
             if self.shutterswitch is False:
@@ -467,7 +467,7 @@ class mesoSPIM_Core(QtCore.QObject):
                 self.shutter_right.open() # set side-switch to true
         else: # BOTH open
             self.shutter_right.open()
-            self.shutter_left.open()
+            #self.shutter_left.open()
 
         self.state['shutterstate'] = True
 
@@ -479,7 +479,7 @@ class mesoSPIM_Core(QtCore.QObject):
         and the shutter_right line is the left/right switch (Right==True)
         '''
         if self.shutterswitch is False:
-            self.shutter_left.close()
+          #  self.shutter_left.close()
             self.shutter_right.close()
         else:
             self.shutter_left.close()
