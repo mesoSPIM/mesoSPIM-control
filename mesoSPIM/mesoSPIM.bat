996a1a64efc35e0c253a7d8269a9895b9be17a64
Nikita Vladimirov <nvladimus@gmail.com>
2024-12-06 11:51:52 +0100

    update default config files

--- mesoSPIM/mesoSPIM.bat
+++ mesoSPIM/mesoSPIM.bat
@@ -1,11 +1,8 @@
 echo off
-echo Setting up your conda enviroment:
-echo Change the Anaconda path "...\activate.bat" to your own
-echo To print your Anaconda path, run this in Anaconda prompt:
-echo $ where conda
-echo In a multi-user setting, you can create the py37 environment in a public folder accessible for all users:
-echo $ conda create -p C:/Users/Public/conda/envs/mesoSPIM-py37 python=3.7
-echo Make sure all users have right to execute python in this py37 environment (admin rights required).
+echo Setting up your Mamba enviroment:
+echo Change the Mamba path "...\activate.bat" to your own
+echo To print your Mamba path, run this in Miniforge prompt:
+echo $ where mamba
 echo off
-"%windir%\System32\cmd.exe" /k ""C:\Users\Nikita\anaconda3\Scripts\activate.bat" "C:\Users\Public\conda\envs\mesoSPIM-py37" && python "mesoSPIM_Control.py""
+"%windir%\System32\cmd.exe" /k ""C:\ProgramData\miniforge3\Scripts\activate.bat" "C:\Users\Public\mamba\envs\mesoSPIM-py312" && python "mesoSPIM_Control.py""
 pause
