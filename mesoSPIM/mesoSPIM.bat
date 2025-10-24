3da4732c74d529454ca9aff95fc53ca19f2409ab
Alan Watson <AlphaWhiskey82@yahoo.com>
2025-10-24 17:52:45 -0400

    Restore MesoSPIM.bat

--- mesoSPIM/mesoSPIM.bat
+++ mesoSPIM/mesoSPIM.bat
@@ -4,5 +4,5 @@ echo Change the Mamba path "...\activate.bat" to your own
 echo To print your Mamba path, run this in Miniforge prompt:
 echo $ where mamba
 echo off
-"%windir%\System32\cmd.exe" /k ""C:\Users\lab\miniconda3\condabin\activate.bat" "C:\Users\lab\miniconda3\envs\mesoSPIM_dev" && python "mesoSPIM_Control.py""
+"%windir%\System32\cmd.exe" /k ""C:\ProgramData\miniforge3\Scripts\activate.bat" "C:\Users\Public\mamba\envs\mesoSPIM-py312" && python "mesoSPIM_Control.py""
 pause
